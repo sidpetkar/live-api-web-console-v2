5fc4b1f514488303384ec950d57d30d45e2ef5b2
Siddhant Petkar <siddhantpetkar@gmail.com>
2025-04-26 19:26:45 +0530

    Feat: Optimize for mobile (hide screen share, side panel, use rear cam)

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@
 import { useRef, useState } from "react";
 import "./App.scss";
 import { LiveAPIProvider } from "./contexts/LiveAPIContext";
-import SidePanel from "./components/side-panel/SidePanel";
+// import SidePanel from "./components/side-panel/SidePanel";
 import { Altair } from "./components/altair/Altair";
 import ControlTray from "./components/control-tray/ControlTray";
 import cn from "classnames";
@@ -41,7 +41,7 @@ function App() {
     <div className="App">
       <LiveAPIProvider url={uri} apiKey={API_KEY}>
         <div className="streaming-console">
-          <SidePanel />
+          {/* <SidePanel /> */}
           <main>
             <div className="main-app-area">
               {/* APP goes here */}
